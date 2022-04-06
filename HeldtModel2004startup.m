35086957f10b43b344a8bff8110cef4e4b14fdb6
ValentinSiderskiyPhD <56981028+ValentinSiderskiyPhD@users.noreply.github.com>
2022-04-06 10:36:09 -0400

    Started 2004 model - 21 compartments

---
+++ HeldtModel2004startup.m
@@ -0,0 +1,16 @@
+%Arterial baroreflex
+SP_artb = 98;  %Arterial baroreflex set-point pressure mm Hg (98±1)
+SF_artb = 18;% Arterial baroreflex scaling factor mm Hg 18
+G_RR_sym = 9; %R-R-interval sympathetic gain ms/mmHg (9±1)
+G_RR_par = 9; %R-R-interval parasympathetic gain ms/mmHg (9±1)
+G_artb_R_up =  -0.05; %Arterial baroreflex Rup gain PRU/mmHg (-0.05±0.01)
+G_artb_R_k =  -0.05; %Arterial baroreflex Rk gain PRU/mm Hg (-0.05±0.01)
+G_artb_R_sp = -0.05;  %Arterial baroreflex Rsp gain PRU/mm Hg (-0.05±0.01)
+G_artb_R_ll = -0.05; %Arterial baroreflex Rll gain PRU/mmHg (-0.05±0.01)
+G_artb_V_up = 5; %Arterial baroreflex Vup gain ml/mmHg (5±2)
+G_artb_V_k = 2; %Arterial baroreflex Vk gain ml/mmHg (2±1)
+G_artb_V_sp = 13; %Arterial baroreflex Vsp gain ml mm Hg (13±3)
+G_artb_V_ll = 7; %Arterial baroreflex Vll gain ml/mmHg (7±2)
+G_artb_C_es_rv = 0.022; % Arterial baroreflex Ces_rv gain ml/(mmHg)^2 (0.022±0.003)
+G_artb_C_es_lv = 0.007l % Arterial baroreflex C_es_lv gain ml/mmHg2 (0.007±0.001)
+
